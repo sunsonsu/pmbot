7c7d3f20781d9a703779bcc33ad74cf2a3e161fb
sunsonsu <komson281@gmail.com>
2025-03-17 19:01:42 +0700

    bug fix for vercel

--- api/index.js
+++ api/index.js
@@ -165,8 +165,5 @@ async function getPM25() {
         return { pm25: "N/A", place: "Unknown", time: "N/A" };
     }
 }
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
+
 module.exports = app;
