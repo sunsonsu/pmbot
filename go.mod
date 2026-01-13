3e02d6b75cd30964c5a8a4a507d559037fa8c4bc
sunsonsu <komsan.works@gmail.com>
2026-01-14 00:46:16 +0700

    go version 1.0

---
+++ go.mod
@@ -0,0 +1,8 @@
+module pmbot
+
+go 1.25.5
+
+require (
+	github.com/aws/aws-lambda-go v1.51.2 // indirect
+	github.com/line/line-bot-sdk-go/v8 v8.18.0 // indirect
+)
--- zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ZIP_NAME="lambda_function.zip"
-
-rm -f $ZIP_NAME
-
-zip -r $ZIP_NAME index.js flexTemplate.js node_modules package.json
-
-echo "Zip Done $ZIP_NAME"
